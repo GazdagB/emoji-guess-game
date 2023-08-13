9c436e34554b1a0179766c879e2e2cb52d3b1328
The Code Designer <gazdagbal@gmail.com>
2023-08-13 19:44:17 +0200

    First Prototype v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import allEmojiData from './all-emoji.json';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [submitedInput,setSubmitedInput] = useState(""); 
+  const [emojiData, setEmojiData] = useState(allEmojiData);
+  const [randomIndex, setRandomIndex] = useState(0); // Initialize with 0
+  const [life,setLife] = useState(3);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setSubmitedInput(input);
+  }
+
+  function handleSkip(){
+    setInput("");
+    setSubmitedInput("");
+    setRandomIndex(Math.floor(Math.random() * emojiData.length));
+    setLife((prevLife) => prevLife - 1);
+  }
+
+
+  useEffect(() => {
+    const getRandomInt = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    setRandomIndex(getRandomInt(0, emojiData.length - 1));
+  }, [emojiData]); // Execute whenever emojiData changes
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="emoji-game">
+       <h1>Guess The Movie by the Emojis</h1>
+        <h2 className='emoji-game-emojies'>
+        {emojiData[randomIndex].Emojies}
+        </h2>
+        <form action="">
+        <input placeholder='What movie is this...' maxLength={50} value={input} onChange={(e) => setInput(e.target.value)} />
+        <button className='primary' onClick={handleSubmit}>Guess</button>
+        </form>
+       <h3>{emojiData[randomIndex].Title.toLocaleLowerCase() === submitedInput.toLocaleLowerCase() ? `Correct the answer is: ${emojiData[randomIndex].Title}` : ""}</h3>
+       <button className='secondary' onClick={handleSkip}>Skip</button>
+      </div>
+      
     </div>
   );
 }
